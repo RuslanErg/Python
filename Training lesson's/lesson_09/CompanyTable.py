f48074178a6b0fad577ce1a17ae9bb28c6b27a91
RuslanErg <ruslan.bel.2013@gmail.com>
2025-07-22 22:56:56 +0300

    HW9

--- Training lesson's/lesson_09/CompanyTable.py
+++ Training lesson's/lesson_09/CompanyTable.py
@@ -1,13 +1,19 @@
 from sqlalchemy import create_engine, text
+from datetime import datetime
 
 
 class CompanyTable:
     __scripts = {
         "select": text("select * from company where deleted_at is null"),
-        "select active": text("select * from company where \"is_active\" = true AND deleted_at is null"),
-        "delete by id": text("delete from company where id =:id_to_delete"),
-        "insert_new": text("INSERT INTO company(\"name\") values (:new_name)"),
-        "get_max_id": text("SELECT MAX(\"id\") FROM company WHERE deleted_at IS NULL")
+        "select_active": text("select * from company where \"is_active\" = true AND deleted_at is null"),
+        "delete_by_id": text("delete from company where id =:id_to_delete"),
+        "insert_new": text("""
+        INSERT INTO company("name", "is_active", "create_timestamp", "change_timestamp")
+        VALUES (:name, :is_active, :create_timestamp, :change_timestamp)
+    """),
+        "get_max_id": text("SELECT MAX(\"id\") FROM company WHERE deleted_at IS NULL"),
+        "select by id": text("SELECT \* FROM company "
+                             "WHERE id =:select_id AND deleted_at IS NULL")
     }
 
     def __init__(self, connection_string):
@@ -22,20 +28,24 @@ class CompanyTable:
 
     def get_active_companies(self):
         conn = self.__db.connect()
-        result = conn.execute(self.__scripts["select active"])
+        result = conn.execute(self.__scripts["select_active"])
         rows = result.mappings().all()
         conn.close()
         return rows
 
     def delete(self, id):
         conn = self.__db.connect()
-        conn.execute(self.__scripts ["delete by id"], {"id_to_delete": id})
+        conn.execute(self.__scripts ["delete_by_id"], {"id_to_delete": id})
+        conn.commit()
         conn.close()
 
-
-    def create(self, name):
+    def create(self, name, is_active, create_timestamp, change_timestamp):
+        if create_timestamp is None:
+            create_timestamp = datetime.now()
+            change_timestamp = datetime.now()
         conn = self.__db.connect()
-        conn.execute(self.__scripts ["insert_new"], {"new_name" : name})
+        conn.execute(self.__scripts["insert_new"],
+                     {"new_name": name, "is_active": is_active, "create_timestamp": create_timestamp, "change_timestamp": change_timestamp})
         conn.commit()
         conn.close()
 
@@ -46,3 +56,22 @@ class CompanyTable:
         max_id = result.scalar()
         conn.close()
         return max_id
+
+    def get_company_by_id(self, id):
+        conn = self.__db.connect()
+        result = conn.execute(
+            self.__scripts["select by id"],{"select_id": id}
+        )
+        company = result.mappings().all()
+        conn.close()
+        return company
+
+    def get_company_by_id(self, id):
+        conn = self.__db.connect()
+        result = conn.execute(
+            self.__scripts["select by id"],
+        {"select_id": id}
+        )
+        company = result.mappings().all()
+        conn.close()
+        return company
